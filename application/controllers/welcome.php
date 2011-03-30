a9eb193ffaf3e2e962166852925df198bcd76162
Chris <cleblanc87@gmail.com>
2011-03-29 22:51:10 -0600

    Doctrine n such

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -20,6 +20,13 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
           $data['ci_msg_str'] = 'looks like we need to use the php tags unless we can incorporate tpl :O. Also inspect this element<? o cool php tags in the data array become comment s?>';
+          $u = new User;
+          $u->username = 'johndoe';
+	  $u->password = 'secret';
+	  $u->first_name = 'John';
+	  $u->last_name = 'Doe';
+	  $u->save();
+
 	  $this->load->view('welcome_message', $data);
 	}
 }
